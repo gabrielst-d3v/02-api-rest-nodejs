9105a4c4ed7d5ed593565530849096517e5c0fa9
Gabriel Santos <ebert.gabrielst@gmail.com>
2024-12-23 13:37:18 -0300

    :tada: Creating application

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+// GET, POST, PATCH, DELETE
+
+// https://localhost:3333/hello
+
+app.get('/hello', () => {
+  return 'Hello World!'
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server Running!')
+})
